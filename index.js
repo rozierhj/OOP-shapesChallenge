09ee357583b0c26a75ffd40014e6073401f53d1a
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-05-28 23:52:18 -0400

    comments were added

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const fs = require('fs').promises;
-const color = require('color');
 const inquirer = require('inquirer');
 const {Triangle, Circle, Square} = require('./lib/shapes');
 const { type } = require('os');
 const validateColor = require('validate-color');
 
 const userInput = [
-    {
-        type: 'input',
-        name: 'colors',
-        message: 'Enter a color for your logo: ',
-    },
-    {
+    {//get the three letters for the logo name
         type: 'input',
         name: 'name',
-        message: 'Enter the name for your logo: ',
+        message: 'Enter the name for your logo (use 3 characters): ',
     },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape for your logo: ',
-        choices: ['Square','Circle','Triangle'],
-    },
-    {
+    {//get the color for the logo text
         type: 'input',
         name: 'textColor',
-        message: 'Choose a color for the font: ',
+        message: 'Choose a color for the logo text: ',
+    },
+    {//get the shape of the logo
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for the logo: ',
+        choices: ['Square','Circle','Triangle'],
+    },
+    {//get the color of the logo background
+        type: 'input',
+        name: 'colors',
+        message: 'Enter a color for your logo shape: ',
     },
 ];
 
@@ -35,18 +34,18 @@ function getInput(){
 
     inquirer.prompt(userInput)
     .then(data =>{
-        console.log(`User choices: ${data.colors}, ${data.name} and ${data.shape}`);
 
+        //define Circle object type and render the code prepared for the .svg file for it
         if(data.shape === 'Circle'){
             const circle = new Circle(data.colors, data.name, data.shape, data.textColor);
             resolve(circle.render());
         }
-
+        //defined the Triangle object type and render the code prepared for the .svg file for it
         else if(data.shape === 'Triangle'){
             const triangle = new Triangle(data.colors, data.name, data.shape, data.textColor);
             resolve(triangle.render());
         }
-
+        //defined the Square object type and render the code prepared for the .svg file for it
         else if(data.shape === 'Square'){
             const square = new Square(data.colors, data.name, data.shape, data.textColor);
             resolve(square.render());
@@ -63,9 +62,15 @@ function getInput(){
 
 }
 
+//execute the function to get the user input for the logo
 getInput()
 .then(data =>{
-    return fs.writeFile('./examples/testShape.svg',data);
+    //using the code returned from the logo rendering for the Circle, Triangle or Square object type, now write that data to a .svg file
+    return fs.writeFile('./examples/logo.svg',data);
+})
+.then(data =>{
+    //display message confirming that the file has been created.
+    console.log(`Generated logo.svg`);
 })
 .catch(error =>{
     console.error('Error:',error.message)

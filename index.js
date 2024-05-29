92414c7534720c7e8f89e8ff2d1a1788eae4dc35
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-05-28 20:21:38 -0400

    update system to return error with bade shape value (just for a test)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const color = require('color');
 const inquirer = require('inquirer');
-const Shapes = require('./lib/shapes');
+const {Triangle, Circle, Square} = require('./lib/shapes');
 const { type } = require('os');
 
 const userInput = [
@@ -19,24 +19,50 @@ const userInput = [
         type: 'list',
         name: 'shape',
         message: 'Choose a shape for your logo: ',
-        choices: ['Square','Circle','Triangle'],
+        choices: ['Square','Circle','Triangle', 'test'],
     }
 ];
 
 function getInput(){
+    
+    return new Promise((resolve, reject) =>{
+
     inquirer.prompt(userInput)
     .then(data =>{
         console.log(`User choices: ${data.colors}, ${data.name} and ${data.shape}`);
-        const shape = new Shapes(data.colors, data.name, data.shape);
-        console.log(shape);
-        return shape.render();
-    })
-    .then(data =>{
-        return fs.writeFile('./examples/testShape.svg',data);
+
+        if(data.shape === 'Circle'){
+            const circle = new Circle(data.colors, data.name, data.shape);
+            resolve(circle.render());
+        }
+
+        else if(data.shape === 'Triangle'){
+            const triangle = new Triangle(data.colors, data.name, data.shape);
+            resolve(triangle.render());
+        }
+
+        else if(data.shape === 'Square'){
+            const square = new Square(data.colors, data.name, data.shape);
+            resolve(square.render());
+        }
+        else{
+             reject(new Error('No shape input was found'));
+        }
+
+        //console.log(shape);
+        
     })
     .catch(error =>{
-        console.error(error);
+        reject(error);
     });
+});
+
 }
 
-getInput();
+getInput()
+.then(data =>{
+    return fs.writeFile('./examples/testShape.svg',data);
+})
+.catch(error =>{
+    console.error('Error:',error.message)
+});
